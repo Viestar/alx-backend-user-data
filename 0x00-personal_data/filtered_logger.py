c42e2e0e29cd7308872ea8335c3a02389cce394a
Viestar <m.silverbuckson@gmail.com>
2024-01-22 13:25:55 +0300

    Pycodestyle

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -14,20 +14,7 @@ PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
 def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
-    '''
-        function that obfuscates personal information
-
-        Arguments:
-        fields: a list of strings representing all fields to
-        obfuscate
-        redaction: a string representing by what the field will
-        be obfuscated
-        message: a string representing the log line
-        separator: a string representing by which character is
-        separating all fields in the log line (message)
-
-        Return: String with redacted values
-    '''
+    ''' Function that obfuscates personal information '''
     for field in fields:
         message = re.sub(f'{field}=.*?{separator}',
                          f'{field}={redaction}{separator}', message)
