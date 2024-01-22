cb48a737dfe2211dc5ba778bb380d3539b10493e
Viestar <m.silverbuckson@gmail.com>
2024-01-22 15:15:50 +0300

    database

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -8,7 +8,7 @@ import logging
 import re
 from os import environ
 
-
+# Picked the user_data.csv file
 PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
 
@@ -37,18 +37,14 @@ class RedactingFormatter(logging.Formatter):
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
-        '''
-            filter values in incoming log records using filter_datum
-        '''
+        '''filter values in incoming log records using filter_datum '''
         record.msg = filter_datum(self.fields, self.REDACTION,
                                   record.getMessage(), self.SEPARATOR)
         return super(RedactingFormatter, self).format(record)
 
 
 def get_logger() -> logging.Logger:
-    '''
-        retrieves logger object
-    '''
+    '''  Retrieves logger object '''
     logger = logging.getLogger("user_data")
     logger.setLevel(logging.INFO)
     logger.propagate = False
