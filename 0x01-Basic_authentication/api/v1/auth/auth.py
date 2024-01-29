1d625bf46b2422e445d1a6a66d077c19d6af489b
Viestar <m.silverbuckson@gmail.com>
2024-01-29 07:31:49 +0300

    More jobs in the function

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -7,15 +7,20 @@ from typing import List, TypeVar
 
 class Auth:
     """ Class for Authorisation on requirements, headers and users"""
+
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ Returns False, For now, always until further implementation"""
         if excluded_paths is None or path is None:
             return True
-        elif path not in excluded_paths:
-            return True
         elif path in excluded_paths:
             return False
-        return False
+        # Ensures slash tolerance
+        path = path.rstrip('/') + '/'  # Ensure path ends with a slash
+        # Ensure excluded_paths end with a slash
+        excluded_paths = [p.rstrip('/') + '/' for p in excluded_paths]
+
+        # Returns False if path is in excluded_paths
+        return path not in excluded_paths
 
     def authorization_header(self, request=None) -> str:
         """ Returns None, For now, always until further implementation """
