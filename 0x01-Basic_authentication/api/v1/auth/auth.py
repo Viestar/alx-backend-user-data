8670a089aaf3939b697a1ec521b0469d8299073b
Viestar <m.silverbuckson@gmail.com>
2024-01-29 07:12:57 +0300

    Documentation

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-""" Module for Authorisation """
+""" Module for Authorisation on requirements, headers and users"""
 
 from flask import request
 from typing import List, TypeVar
@@ -7,15 +7,15 @@ from typing import List, TypeVar
 
 class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """ Returns False """
+        """ Returns False, For now, always until further implementation"""
         return False
 
 
     def authorization_header(self, request=None) -> str:
-        """ Returns None """
+        """ Returns None, For now, always until further implementation """
         return None
     
 
     def current_user(self, request=None) -> TypeVar('User'):
-        """ Returns None """
+        """ Returns None, For now, always until more implementation """
         return None
